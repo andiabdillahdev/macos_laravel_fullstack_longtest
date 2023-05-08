6009dd7e35f6302baec79a9470b3068af66e1d6c
andiabdillahdev <andiabdillahdev@gmail.com>
2023-05-08 16:16:06 +0800

    menambahkan route dan access logic pada books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -29,6 +29,44 @@ class BookController extends BaseController
         return $this->sendResponse($task,'Books Fetched Success');
     }
 
-    
+    public function store(BooksRequest $request){
+        $task = array();
+        try {
+            $task = $this->orderRepository->store($request->all());
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), $e->getMessage(),400);
+        }
+        return $this->sendResponse($task,'Books Added success');
+    }
+
+    public function show($params){
+        $task = array();
+        try {
+            $task = $this->orderRepository->getOne($params);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), $e->getMessage(),400);
+        }
+        return $this->sendResponse($task,'Books Show Success'); 
+    }
+
+    public function update(BooksRequest $request, $params){
+        $task = array();
+        try {
+            $task = $this->orderRepository->update($request->all(), $params);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), $e->getMessage(),400);
+        }
+        return $this->sendResponse($task,'Books Updated success');
+    }
+
+    public function delete(Request $request, $params){
+        $task = array();
+        try {
+            $task = $this->orderRepository->delete($request, $params);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), $e->getMessage(),400);
+        }
+        return $this->sendResponse($task,'Books Deleted success');
+    }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,18 @@ Route::group(['namespace' => 'App\Http\Controllers'], function(){
 
     Route::group(['middleware' => ['admin']], function() {
         Route::get('/dashboard-admin', 'HomeController@dashboard_admin')->name('dashboard.admin');
+
+        Route::prefix('books')->group(function () {
+            Route::get('/', 'BookController@index')->name('profile.index');
+            Route::get('/datatable-list', 'BookController@datatable')->name('profile.datatable');
+            Route::post('/store', 'BookController@store')->name('profile.store');
+            Route::get('/show/{params}', 'BookController@show')->name('profile.show');
+            Route::patch('/update/{params}', 'BookController@update')->name('profile.update');
+            Route::delete('/delete/{params}', 'BookController@delete')->name('profile.delete');
+        });
     });
 
-     Route::group(['middleware' => ['user']], function() {
+    Route::group(['middleware' => ['user']], function() {
         Route::get('/dashboard-user', 'HomeController@dashboard_user')->name('dashboard.user');
     });
 });
