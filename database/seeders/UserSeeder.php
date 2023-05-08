d966d19539dc29ada8a0d8499dde336284cee488
andiabdillahdev <andiabdillahdev@gmail.com>
2023-05-08 13:29:03 +0800

    menambahkan file seeder user admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Hash;
+use DB;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'nama' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);   
+    }
+}
