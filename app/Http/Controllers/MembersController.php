6cde7b6773f88223e40bc344b949bece5b23ade9
andiabdillahdev <andiabdillahdev@gmail.com>
2023-05-08 17:06:14 +0800

    membuat access logic pada members

---
+++ app/Http/Controllers/MembersController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\MembersRequest;
+use App\Http\Controllers\BaseController as BaseController;
+use App\Repository\Members\MembersInterface;
+class MembersController extends BaseController
+{
+        protected MembersInterface $orderRepository;  
+
+    public function __construct(MembersInterface $orderRepository) 
+    {
+        $this->orderRepository = $orderRepository;
+    }
+
+    public function index(){
+        return view('module.members.index');
+    }
+
+    public function datatable(){
+        $task = array();
+        try {
+            $task = $this->orderRepository->getAll();
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), $e->getMessage(),400);
+        }
+        return $this->sendResponse($task,'Members Fetched Success');
+    }
+
+    public function store(MembersRequest $request){
+        $task = array();
+        try {
+            $task = $this->orderRepository->store($request->all());
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), $e->getMessage(),400);
+        }
+        return $this->sendResponse($task,'Members Added success');
+    }
+
+    public function show($params){
+        $task = array();
+        try {
+            $task = $this->orderRepository->getOne($params);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), $e->getMessage(),400);
+        }
+        return $this->sendResponse($task,'Members Show Success'); 
+    }
+
+    public function update(MembersRequest $request, $params){
+        $task = array();
+        try {
+            $task = $this->orderRepository->update($request->all(), $params);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), $e->getMessage(),400);
+        }
+        return $this->sendResponse($task,'Members Updated success');
+    }
+
+    public function delete(Request $request, $params){
+        $task = array();
+        try {
+            $task = $this->orderRepository->delete($request, $params);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), $e->getMessage(),400);
+        }
+        return $this->sendResponse($task,'Members Deleted success');
+    }
+}
