c0a55ae7a636ec1eb1d26028c3c4f199c4f60bd4
andiabdillahdev <andiabdillahdev@gmail.com>
2023-05-08 17:05:56 +0800

    membuat logic fitur members

--- app/Repository/Members/MembersRepository.php
+++ app/Repository/Members/MembersRepository.php
@@ -2,28 +2,30 @@
 
 namespace App\Repository\Members;
 
-use App\Repository\Books\MembersInterface;
-use App\Models\Books;
+use App\Repository\Members\MembersInterface;
+use App\Models\Members;
 
 class MembersRepository implements MembersInterface {
 
     public function getAll(){
-
+        return Members::select('id','name','address','city','phone_number','date_of_birth')->latest()->get();
     }
 
     public function getOne($params){
-        
+        return Members::select('id','name','address','city','phone_number','date_of_birth')->where('id',$params)->first();
     }
 
-    public function store($req){
-        
+     public function store($req){
+         return Members::create($req);
+      
     }
 
-    public function update($req, $params){
-        
+     public function update($req, $params){
+        return Members::where('id', $params)
+        ->update($req);
     }
 
     public function delete($req, $params){
-        
+        return Members::where('id',$params)->delete();
     }
 }
