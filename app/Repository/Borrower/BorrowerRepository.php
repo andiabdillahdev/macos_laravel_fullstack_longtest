3c57cce82145587fa4915e3f3c45f9c5e87268ab
andiabdillahdev <andiabdillahdev@gmail.com>
2023-05-08 18:43:55 +0800

    perbaikan logic get all pada borrowers

--- app/Repository/Borrower/BorrowerRepository.php
+++ app/Repository/Borrower/BorrowerRepository.php
@@ -2,13 +2,18 @@
 
 namespace App\Repository\Borrower;
 
-use App\Repository\Books\BorrowerInterface;
+use App\Repository\Borrower\BorrowerInterface;
 use App\Models\Borrower;
 use DB;
 class BorrowerRepository implements BorrowerInterface {
 
     public function getAll(){
-        return DB::table('borrower')->select('borrower.id','borrower.id_members','borrower.book_number','borrower.loan_date','borrower.date_of_return','borrower.status','members.name as members','books.title')->join('members','borrower.id_members','=','members.id')->join('books','borrower.book_number','=','books.id')->latest()->get();
+        return 
+        DB::table('borrower')
+        ->select('borrower.id','borrower.id_members','borrower.book_number','borrower.loan_date','borrower.date_of_return','borrower.status','members.name as members','books.title')
+        ->join('members','borrower.id_members','=','members.id')
+        ->join('books','borrower.book_number','=','books.book_number')
+        ->get();
     }
 
     public function getOne($params){
