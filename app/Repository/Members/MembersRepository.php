ccbed0a3e3bd618e7845867e2214bd8f89cf3d06
andiabdillahdev <andiabdillahdev@gmail.com>
2023-05-08 17:47:38 +0800

    membuat logic fitur members

--- app/Repository/Members/MembersRepository.php
+++ app/Repository/Members/MembersRepository.php
@@ -4,19 +4,40 @@ namespace App\Repository\Members;
 
 use App\Repository\Members\MembersInterface;
 use App\Models\Members;
-
+use Hash;
+use DB;
 class MembersRepository implements MembersInterface {
 
     public function getAll(){
-        return Members::select('id','name','address','city','phone_number','date_of_birth')->latest()->get();
+        return Members::select('id','name','address','city','email','phone_number','date_of_birth')->latest()->get();
     }
 
     public function getOne($params){
-        return Members::select('id','name','address','city','phone_number','date_of_birth')->where('id',$params)->first();
+        return Members::select('id','name','address','city','email','phone_number','date_of_birth')->where('id',$params)->first();
     }
 
-     public function store($req){
-         return Members::create($req);
+     public function store($req){ 
+        $data = new Members();
+        $data->name = $req['name'];
+        $data->address = $req['address'];
+        $data->city = $req['city'];
+        $data->email = $req['email'];
+        $data->phone_number = $req['phone_number'];
+        $data->date_of_birth = $req['date_of_birth'];
+        $data->save();
+
+
+        if ($data) {
+            DB::table('users')->insert([
+                'nama' => $data->name,
+                'email' => $data->email,
+                'password' => Hash::make($data->email),
+                'role' => 'user',
+                'id_members' => $data->id
+            ]);
+        }
+
+        return $data;   
     }
 
      public function update($req, $params){
