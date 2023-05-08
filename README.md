920710b2a63f8ba258be8c7f79367f672533feb5
andiabdillahdev <andiabdillahdev@gmail.com>
2023-05-08 19:15:30 +0800

    readme dan file database

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# macos_laravel_fullstack_longtest
+# macos_laravel_fullstack_longtest ( Sistem perpustakaan )
+
+## Instruksi
+
+ini adalah project test untuk fullstack developer di PT Ahli Bangun Sistem
+
+- Pertama, buka direktori tempat project anda, lalu buka terminal.
+- Ketik perintah git clone https://github.com/andiabdillahdev/macos_laravel_fullstack_longtest.git
+- Jika sudah selesai clone, akses folder projectnya lalu ketik perintah melalui terminal composer install
+- buat folder .env di direktori app laravelnya
+- jalankan php artisan migrate untuk meng-generate table atau import file sql yang telah saya sertakan di direktory project
+- jalankan php artisan db:seed --class=UserSeeder untuk membuat akun admin
+- Jalankan app laravel dengan php artisan serve.
+
+## NOTE
+
+aplikasi ini terdapat 2 role pengguna yaitu admin dan user. email dan password user defaultnya berdasarkan email yang di input oleh admin.
---
+++ perpustakaan19-14.sql
@@ -0,0 +1,237 @@
+﻿# ************************************************************
+# Sequel Ace SQL dump
+# Version 20033
+#
+# https://sequel-ace.com/
+# https://github.com/Sequel-Ace/Sequel-Ace
+#
+# Host: localhost (MySQL 8.0.30)
+# Database: perpustakaan
+# Generation Time: 2023-05-08 11:14:12 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE='NO_AUTO_VALUE_ON_ZERO', SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table books
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `books`;
+
+CREATE TABLE `books` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `book_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `author` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `year` year NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `books_book_number_unique` (`book_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+
+INSERT INTO `books` (`id`, `book_number`, `title`, `author`, `year`, `created_at`, `updated_at`)
+VALUES
+	(1,'EetxYWLy','tes judul 123','andi','2022','2023-05-08 08:40:51','2023-05-08 08:53:07'),
+	(3,'JOLRXhhd','Harry potter','andi','1998','2023-05-08 10:39:40','2023-05-08 10:39:40'),
+	(4,'UUClhcCM','Tenggelamnya kapal van der wich','andi','2001','2023-05-08 10:39:59','2023-05-08 10:39:59');
+
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table borrower
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `borrower`;
+
+CREATE TABLE `borrower` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id_members` bigint unsigned NOT NULL,
+  `book_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `loan_date` date NOT NULL,
+  `date_of_return` date NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `borrower_id_members_foreign` (`id_members`),
+  CONSTRAINT `borrower_id_members_foreign` FOREIGN KEY (`id_members`) REFERENCES `members` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `borrower` WRITE;
+/*!40000 ALTER TABLE `borrower` DISABLE KEYS */;
+
+INSERT INTO `borrower` (`id`, `id_members`, `book_number`, `loan_date`, `date_of_return`, `status`, `created_at`, `updated_at`)
+VALUES
+	(1,9,'EetxYWLy','2023-05-08','2023-05-08',0,'2023-05-08 10:34:53','2023-05-08 10:34:53'),
+	(2,10,'JOLRXhhd','2023-05-08','2023-05-17',0,'2023-05-08 10:40:37','2023-05-08 10:40:37');
+
+/*!40000 ALTER TABLE `borrower` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table failed_jobs
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `failed_jobs`;
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Dump of table members
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `members`;
+
+CREATE TABLE `members` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `members` WRITE;
+/*!40000 ALTER TABLE `members` DISABLE KEYS */;
+
+INSERT INTO `members` (`id`, `name`, `address`, `city`, `phone_number`, `date_of_birth`, `created_at`, `updated_at`, `email`)
+VALUES
+	(9,'Prof. Noe Waters','Perumahan pesona griya','Bali','081936098005','2023-05-08','2023-05-08 09:42:02','2023-05-08 09:46:12','admin@admin.com'),
+	(10,'Rum salengke','Perumahan pesona griya','Makassar','081776554336','2023-05-08','2023-05-08 10:40:25','2023-05-08 10:40:25','andiabdillahdev@gmail.com');
+
+/*!40000 ALTER TABLE `members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table migrations
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`)
+VALUES
+	(1,'2014_10_12_000000_create_users_table',1),
+	(2,'2014_10_12_100000_create_password_resets_table',1),
+	(3,'2019_08_19_000000_create_failed_jobs_table',1),
+	(4,'2019_12_14_000001_create_personal_access_tokens_table',1),
+	(5,'2023_05_08_040420_create_books_table',1),
+	(6,'2023_05_08_040436_create_members_table',1),
+	(7,'2023_05_08_040655_create_borrower_table',1);
+
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table password_resets
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `password_resets`;
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Dump of table personal_access_tokens
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `personal_access_tokens`;
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` enum('admin','user') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `id_members` bigint unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`id`, `nama`, `email`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`, `id_members`)
+VALUES
+	(1,'admin','admin@gmail.com',NULL,'$2y$10$fPLJ05UwbJjudY7bYv0Wx.PCtEsQskODoIk2Ri1WcqsWaxnkqV60G','admin',NULL,NULL,NULL,0),
+	(2,'Prof. Noe Waters','admin@admin.com',NULL,'$2y$10$z0.Q5/01yXQ/CF0XI1H7O.IzPICf/zB1mJBlHzEftbEV.EApS6Cky','user',NULL,NULL,NULL,9),
+	(3,'Rum salengke','andiabdillahdev@gmail.com',NULL,'$2y$10$5TBivj2n/Hak8QHOkonxeeG45GaBCrMpwb23xrz5xtDx5h9BqBk0q','user',NULL,NULL,NULL,10);
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
