bc1f7d5493edfa0aebd1e56cd6946a30464285aa
andiabdillahdev <andiabdillahdev@gmail.com>
2023-05-08 13:40:05 +0800

    menambahkan kolom di tabel user di migration

---
+++ database/migrations/2023_05_08_053412_add_members_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMembersToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_members');
+            $table->foreignId('id_members')->nullable()->constrained('members');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
